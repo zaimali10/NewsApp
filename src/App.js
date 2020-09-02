c86ed8ad73445d1abccc36cbc2a975ec866e9b32
Zaim <zaimali10@yahoo.com>
2020-09-02 16:02:48 -0500

    Added Alan logo to 'homepage' along with cards with user instructions on how to use app with styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from './styles.js'
 
 const alanKey =
   "a537c0c122f11b4c419768c38fef10e52e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
+  const classes = useStyles();
 
   //if you leave the array at the end of of useEffect empty, then useEffect will only run once and itll run when the page is first loaded
   useEffect(() => {
@@ -23,7 +25,9 @@ const App = () => {
 
   return (
     <div>
-      <h1> Alan AI News Application </h1>
+      <div className={classes.logoContainer}>
+        <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+      </div>
       <NewsCards articles={newsArticles} />
     </div>
   );

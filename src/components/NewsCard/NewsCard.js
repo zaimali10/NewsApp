3c02fc0772022123291c660986e853edc7d062b0
Zaim <zaimali10@yahoo.com>
2020-09-02 15:49:18 -0500

    Added page to show users how to use the webpage, not styled yet

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -5,19 +5,19 @@ import useStyles from './styles.js'
 const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i }) => {
   const classes = useStyles();
   return (
-    <Card>
-      <CardActionArea>
+    <Card className={classes.card}>
+      <CardActionArea href={url} target="_blank" /*Target = blank makes it so when link is clicked it opens a new page and not on top of our current one */>
         <CardMedia className={classes.media} image={urlToImage || 'https://image.flaticon.com/icons/svg/21/21601.svg'} />
-        <div>
+        <div className={classes.details}>
           <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
           <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
         </div>
-        <Typography gutterBottom variant='h5'>{title}</Typography>
+        <Typography className={classes.title} gutterBottom variant='h5'>{title}</Typography>
         <CardContent>
           <Typography variant='body2' color='textSecondary' component='p'>{description}</Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
+      <CardActions className={classes.cardActions}>
         <Button size='small' color='primary'>Learn More</Button>
         <Typography variant='h5' color='textSecondary'>{i + 1}</Typography>
       </CardActions>

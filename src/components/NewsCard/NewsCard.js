0d9adb0c8171fe7a067422697788d7da74fa29bb
Zaim <zaimali10@yahoo.com>
2020-09-02 17:26:47 -0500

    Added scrolling while alan reads the articles, added functionaility that allows users to open an article using the number

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, createRef } from "react";
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
 import classNames from 'classnames';
 
@@ -6,8 +6,21 @@ import useStyles from './styles.js'
 
 const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i, activeArticle }) => {
   const classes = useStyles();
+  const [elRefs, setElRefs] = useState([]);
+  const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+  useEffect(() => {
+    setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+  }, [])
+
+  useEffect(() => {
+    if (i === activeArticle && elRefs[activeArticle]) {
+      scrollToRef(elRefs[activeArticle]);
+    }
+  }, [i, activeArticle, elRefs])
+
   return (
-    <Card className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+    <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
       <CardActionArea href={url} target="_blank" /*Target = blank makes it so when link is clicked it opens a new page and not on top of our current one */>
         <CardMedia className={classes.media} image={urlToImage || 'https://image.flaticon.com/icons/svg/21/21601.svg'} />
         <div className={classes.details}>

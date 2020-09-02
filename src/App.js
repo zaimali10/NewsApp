5f6b85d36d91f641a5c2c456f9a12f2a872427ab
Zaim <zaimali10@yahoo.com>
2020-09-02 14:12:55 -0500

    Started adding Alan functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey =
+  "a537c0c122f11b4c419768c38fef10e52e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  //if you leave the array at the end of of useEffect empty, then useEffect will only run once and itll run when the page is first loaded
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("this code works");
+        }
+      },
+    });
+  }, []);
+
   return (
     <div>
-      <h1>Alan AI News Application</h1>
+      <h1> Alan AI News Application </h1>
     </div>
   );
 };
